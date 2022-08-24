e1c8f6393661e984ef54454e008f00d19315db01
PedroSantana <phsantana99@gmail.com>
2022-08-24 20:27:36 -0300

    requisito 1 finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,2 +1,70 @@
-test('', () => {});
-// iniciando projeto
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando App', () => {
+  it('Testa se a aplicação possui os links no topo', () => {
+    // acessar
+    renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+    const about = screen.getByRole('link', { name: 'About' });
+    const favorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    // aferir
+    expect(home).toBeInTheDocument();
+    expect(about).toBeInTheDocument();
+    expect(favorites).toBeInTheDocument();
+  });
+
+  it('Testa se a aplicação é redirecionada para Home', () => {
+    // acessar
+    const { history } = renderWithRouter(<App />);
+    const home = screen.getByRole('link', { name: 'Home' });
+
+    // agir
+    expect(home).toBeInTheDocument();
+    userEvent.click(home);
+
+    // aferir
+    expect(history.location.pathname).toBe('/');
+  });
+
+  test('Testa se a aplicação é redirecionada para About', () => {
+    // acessar
+    const { history } = renderWithRouter(<App />);
+    const about = screen.getByRole('link', { name: 'About' });
+
+    // agir
+    expect(about).toBeInTheDocument();
+    userEvent.click(about);
+
+    // aferir
+    expect(history.location.pathname).toBe('/about');
+  });
+
+  test('Testa se a aplicação é redirecionada para Favoritos', () => {
+    const { history } = renderWithRouter(<App />);
+    const favorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+
+    // agir
+    expect(favorites).toBeInTheDocument();
+    userEvent.click(favorites);
+
+    // aferir
+    expect(history.location.pathname).toBe('/favorites');
+  });
+
+  test('Testa se a aplicação é redirecionada para Not Found', () => {
+    // acessar
+    const { history } = renderWithRouter(<App />);
+
+    // agir
+    history.push('/*');
+    const notFound = screen.getByText('Page requested not found');
+
+    // aferir
+    expect(notFound).toBeInTheDocument();
+  });
+});
