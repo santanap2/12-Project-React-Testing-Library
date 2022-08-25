dc41d5ad3bb02d068f21c370eb78f082ab5d3ba9
PedroSantana <phsantana99@gmail.com>
2022-08-24 21:05:40 -0300

    requisito 2 arrumado + requisito 3 finalizado

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -28,8 +28,11 @@ describe('Testando About', () => {
     // acessar
     renderWithRouter(<About />);
     const image = screen.getByRole('img', { name: 'Pokédex' });
+    const { src } = image;
+    const URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
 
     // aferir
     expect(image).toBeInTheDocument();
+    expect(src).toBe(URL);
   });
 });
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Testando FavoritePokemons', () => {
+  it('Testa se é exibida na tela a mensagem caso a pessoa não tenha favoritos', () => {
+    // acessar
+    renderWithRouter(<FavoritePokemons />);
+    const notFoundMessage = screen.getByText('No favorite pokemon found');
+    const title = screen.getByRole('heading', { name: 'Favorite pokémons' });
+
+    // aferir
+    expect(notFoundMessage).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
+  });
+});
